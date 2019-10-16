313ad8bfdc20e1220f808c499341c7b84e7bf15d
Joe Hellerstein <jlheller@uw.edu>
2019-10-15 17:45:10 -0700

    ModelRunner passes tests

--- lectures/lecture_8/model_runner.py
+++ lectures/lecture_8/model_runner.py
@@ -12,24 +12,53 @@ TIME = 'time'
 MEAN = 'mean'
 STD = 'std'
 
+# Globals
+runner = None
+
 ############ FUNCTIONS ######################
+def residuals(parameters):
+  """
+  Computes residuals from the parameters using the global runner
+  Residuals are computed based on the last chemical species
+  in the model.
+  :param lmfit.Parameter parameters:
+  """
+  runner.road_runner.reset()  
+  param_dict = parameters.valuesdict()
+  for constant in runner.constants:
+    stmt = "runner.road_runner.%s = param_dict['%s']" % (
+        constant, constant)
+    exec(stmt)
+  sim_data = runner.road_runner.simulate(0,
+       runner.simulation_time, runner.num_points)
+  df_sim_data = pd.DataFrame(sim_data)
+  del df_sim_data[df_sim_data.columns.tolist()[0]]  # Delete time
+  df_sim_data.columns = runner.species
+  df_residuals = runner.df_noisey - df_sim_data
+  ser = df_residuals[df_residuals.columns[-1]]
+  return np.array(ser.tolist())
 
 
 ############ CLASSES ######################
 class ModelRunner(object):
 
-  def __init__(self, model_str, simulation_time, num_points):
+  def __init__(self, model_str, constants,
+      simulation_time, num_points, noise_std=0.5):
     """
     :param str model_str: Antimony model
+    :param list-str constants: list of constants to fit in model
     :param int simulation_time: length of simulation
     :param int num_points: number of data points
     """
     self.model_str = model_str
+    self.constants = constants
     self.simulation_time = simulation_time
     self.num_points = num_points
-    self._makeData()
+    self.noise_std = noise_std
+    self._makeNoiselessData()
 
-  def _makeData(self):
+
+  def _makeNoiselessData(self):
     self.road_runner = te.loada(self.model_str)
     data = self.road_runner.simulate(0,
         self.simulation_time, self.num_points)
@@ -40,44 +69,55 @@ class ModelRunner(object):
     self.df_noiseless = self.df_data[self.df_data.columns[1:]]
     self.species = self.df_noiseless.columns.tolist()
     self.ser_times = self.df_data[TIME]
-    self.df_noisey = pd.DataFrame()
 
-  def generateObservations(self, std):
+  def generateObservations(self, std=None):
     """
     Creates random observations by adding normally distributed
     noise.
-    :param float std:
+    :param float std: if none, use constructor
+    :return pd.DataFrame: noisey data
     """
+    if std is None:
+      std = self.noise_std
     df_rand = pd.DataFrame(np.random.normal(0, std,
          (len(self.df_noiseless),len(self.species))))
     df_rand.columns = self.species
-    self.df_noisey = self.df_noiseless + df_rand
-    self.df_noisey = self.df_noisey.applymap(lambda v:
-        0 if v < 0 else v)
-   
-  def fit(self, count=1, method='leastsq', std=0.5, func=residuals):
+    df = self.df_noiseless + df_rand
+    df = df.applymap(lambda v: 0 if v < 0 else v)
+    return df
+  
+  # FIXME: Allow for specifying min/max for constants 
+  def fit(self, count=1, method='leastsq', std=None, func=None):
     """
     Performs multiple fits.
     :param int count: Number of fits to do, each with different
                       noisey data
-    :return pd.DataFrame: columns are 'MEAN', 'STD',
-                          are species
+    :return pd.DataFrame: columns species; rows are MEAN, STD
     """
-    def runSim(count, method='leastsq', noise_std=0.5, residuals=residuals):
-    # Define the parameters present
-    global y_data
-    constants = {'k1': [], 'k2': [], 'k3': []}
+    if func is None:
+      global runner
+      runner = self
+      func = residuals
+    if std is None:
+      std = self.noise_std
+    #
+    estimates = {}
+    for constant in self.constants:
+        estimates[constant] = []  # Initialize to empty list
     for _ in range(count):
-        for i in range (nDataPoints):
-            y_data[i] = max(data[i, 2] + np.random.normal (0, 5), 0) # standard deviation of noise
-        parameters = lmfit.Parameters()
-        for constant in constants.keys():
-            parameters.add(constant, value=1, min=0, max=10)
-        # Create the minimizer
-        import pdb; pdb.set_trace()
-        fitter = lmfit.Minimizer(residuals, parameters)
-        result = fitter.minimize (method=method)
-        for constant in constants.keys():
-           constants[constant].append(result.params.get(constant).value)
-    return {k: [np.mean(v), np.std(v)] for k,v in constants.items()}
+      self.df_noisey = self.generateObservations(std=std)
+      parameters = lmfit.Parameters()
+      for constant in self.constants:
+          parameters.add(constant, value=1, min=0, max=10)
+      # Create the minimizer
+      fitter = lmfit.Minimizer(residuals, parameters)
+      result = fitter.minimize (method=method)
+      for constant in self.constants:
+        estimates[constant].append(
+           result.params.get(constant).value)
+    df_estimates = pd.DataFrame(estimates)
+    df_result = pd.DataFrame()
+    df_result[MEAN] = df_estimates.mean(axis=0)
+    df_result[STD] = df_estimates.mean(axis=0)
+    return df_result
         
--- lectures/lecture_8/test_model_runner.py
+++ lectures/lecture_8/test_model_runner.py
@@ -1,31 +1,57 @@
+import lmfit
 import model_runner
 import numpy as np
 import pandas as pd
 
 MODEL = """
      A -> B; k1*A
+     B -> C; k2*B
       
      A = 50; 
      B = 0;
+     C = 0;
      k1 = 0.15
+     k2 = 0.25
 """
+CONSTANTS = ['k1', 'k2']
 SIMULATION_TIME = 30
 NUM_POINTS = 5
-COLUMNS = ['time', 'A', 'B']
+COLUMNS = ['time', 'A', 'B', 'C']
 
 def testConstructor():
-  runner = model_runner.ModelRunner(MODEL,
+  runner = model_runner.ModelRunner(MODEL, CONSTANTS,
       SIMULATION_TIME, NUM_POINTS)
   trues = [c in COLUMNS for c in runner.df_data.columns]
   assert(all(trues))
   assert(len(runner.df_noiseless) > 0)
 
 def testGenerateObservations():
-  runner = model_runner.ModelRunner(MODEL,
+  runner = model_runner.ModelRunner(MODEL, CONSTANTS,
       SIMULATION_TIME, NUM_POINTS)
-  runner.generateObservations(0.1)
-  assert(len(set(runner.df_noisey.columns).symmetric_difference(
+  df = runner.generateObservations(0.1)
+  assert(len(set(df.columns).symmetric_difference(
       runner.df_noiseless.columns)) == 0)
-  
 
-testGenerateObservations()
+def testResiduals():
+  runner = model_runner.ModelRunner(MODEL, CONSTANTS,
+      SIMULATION_TIME, NUM_POINTS)
+  runner.df_noisey = runner.generateObservations(0.1)
+  model_runner.runner = runner
+  parameters = lmfit.Parameters()
+  for constant in CONSTANTS:
+    parameters.add(constant, value=1, min=0, max=10)
+  residuals = model_runner.residuals(parameters)
+  assert(len(residuals) == NUM_POINTS)
+
+def testFit():
+  runner = model_runner.ModelRunner(MODEL, CONSTANTS,
+      SIMULATION_TIME, NUM_POINTS)
+  df = runner.fit(count=2)
+  assert(len(df.columns) == 2)
+  assert(len(df) == 2)
+  
+if True:
+  testConstructor()
+  testGenerateObservations()
+  testResiduals()
+  testFit()
