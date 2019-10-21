55699bcd03d437c9ce520b929cb503beb74de75b
Joe Hellerstein <jlheller@uw.edu>
2019-10-21 15:29:02 -0700

    Initial tests for model_fitting

---
+++ lectures/lecture_10/model_fitting.py
@@ -0,0 +1,200 @@
+'''Cross validation codes.'''
+
+import tellurium as te
+import numpy as np
+import lmfit   # Fitting lib
+import math
+import random 
+
+
+
+############## CONSTANTS ######################
+# Default simulation model
+MODEL = """
+     A -> B; k1*A
+     B -> C; k2*B
+      
+     A = 5;
+     B = 0;
+     C = 0;
+     k1 = 0.1
+     k2 = 0.2
+"""
+SIM_TIME = 30
+NUM_POINTS = 10
+CONSTANTS = ['k1', 'k2']
+PARAMETERS = lmfit.Parameters()
+PARAMETERS.add('k1', value=1, min=0, max=10)
+PARAMETERS.add('k2', value=1, min=0, max=10)
+ROAD_RUNNER = None
+#ROAD_RUNNER = te.loada(MODEL)
+
+
+############## FUNCTIONS ######################
+def reshapeData(matrix, indices=None, first_col=0):
+  """
+  Re-structures matrix as an array for just the rows
+  in indices.
+  """
+  if indices is None:
+    nrows = np.shape(matrix)[0]
+    indices = range(nrows)
+  num_columns = np.shape(matrix)[1] - first_col
+  trimmed_matrix = matrix[indices, first_col:]
+  return np.reshape(trimmed_matrix, num_columns*len(indices))
+
+def arrayDifference(matrix1, matrix2, indices=None):
+  """
+  Calculates matrix1 - matrix2 as a nX1 array for the rows
+  specified in indices.
+  """
+  array1 = reshapeData(matrix1, indices=indices)
+  array2 = reshapeData(matrix2, indices=indices)
+  return (array1 - array2)
+
+def calcRsq(observations, estimates, indices=None):
+  """
+  Computes RSQ for simulation results.
+  :param 2d-np.array observations: non-time values
+  :param 2d-np.array estimates: non-time values
+  :param list-int indices:
+  :return float:
+  """
+  array_residuals = arrayDifference(observations, estimates,
+      indices=indices)
+  array_observations = reshapeData(observations, indices=indices)
+  return 1 - np.var(array_residuals)/np.var(array_observations)
+
+def makeParameters(constants=CONSTANTS,
+     values=np.repeat(1, len(CONSTANTS)),
+     mins=np.repeat(0, len(CONSTANTS)),
+     maxs=np.repeat(10, len(CONSTANTS)),
+  ):
+  parameters = lmfit.Parameters()
+  for idx, constant in enumerate(constants):
+    parameters.add(constant,
+        value=values[idx], min=mins[idx], max=maxs[idx])
+  return parameters
+
+def runSimulation(sim_time=SIM_TIME, 
+    num_points=NUM_POINTS, parameters=None,
+    road_runner=ROAD_RUNNER, model=MODEL):
+  """
+  Runs the simulation model rr for the parameters.
+  :param int sim_time: time to run the simulation
+  :param int num_points: number of timepoints simulated
+  :param lmfit.Parameters parameters:
+  :param ExtendedRoadRunner road_runner:
+  :param str model:
+  :return named_array:
+  """
+  if road_runner is None:
+     road_runner = te.loada(model)
+  else:
+    road_runner.reset()
+  if parameters is not None:
+    parameter_dict = parameters.valuesdict()
+    # Set the simulation constants for all parameters
+    for constant in parameter_dict.keys():
+      stmt = "road_runner.%s = parameter_dict['%s']" % (constant, constant)
+      exec(stmt)
+  return road_runner.simulate (0, sim_time, num_points)
+
+def foldGenerator(num_points, num_folds):
+  """
+  Creates generator for test and training data indices.
+  :param int num_points: number of data points
+  :param int num_folds: number of folds
+  :return iterable: Each iteration produces a tuple
+                    First element: training indices
+                    Second element: test indices
+  """
+  indices = range(num_points)
+  for remainder in range(num_folds):
+    test_indices = []
+    for idx in indices:
+      if idx % num_folds == remainder:
+        test_indices.append(idx)
+    train_indices = np.array(
+        list(set(indices).difference(test_indices)))
+    test_indices = np.array(test_indices)
+    yield train_indices, test_indices
+
+def makeObservations(sim_time=SIM_TIME, num_points=NUM_POINTS, noise_std=0.5, **kwargs):
+  """
+  Creates synthetic observations.
+  :param int sim_time: time to run the simulation
+  :param int num_points: number of timepoints simulated
+  :param float noise_std: Standard deviation for random noise
+  :param dict kwargs: keyword parameters used by runSimulation
+  :return namedarray: simulation results with randomness
+  """
+  # Create true values
+  data = runSimulation(sim_time=sim_time, num_points=NUM_POINTS, **kwargs)
+  num_cols = len(data.colnames) - 1
+  # Add randomness
+  for i in range (num_points):
+    for j in range(1, num_cols):
+      data[i, j] = max(obs_data[i, j] + np.random.normal(0, noise_std, 1), 0)
+  return data
+
+def fit(obs_data, indices=None, parameters=PARAMETERS, method='leastsq',
+    **kwargs):
+  """
+  Does a fit of the model to the observations.
+  :param ndarray obs_data: matrix of observed (non-time) values
+  :param list-int indices: indices on which fit is performed
+  :param lmfit.Parameters parameters: parameters fit
+  :param str method: optimization method
+  :param dict kwargs: optional parameters passed to runSimulation
+  :return lmfit.Parameters:
+  """
+  def calcSimulationResiduals(parameters, **kwargs):
+    """
+    Runs a simulation with the specified parameters and calculates residuals
+    for the train_indices.
+    :param lmfit.Parameters parameters:
+    :param dict kwargs: optional parameters passed to simulation
+    """
+    sim_data = runSimulation(parameters=parameters, **kwargs)
+    sim_data = sim_data[:, 1:]  # Skip time
+    return arrayDifference(obs_data, sim_data, indices=indices)
+  # Estimate the parameters for this fold
+  fitter = lmfit.Minimizer(calcSimulationResiduals, parameters)
+  fitter_result = fitter.minimize(method=method)
+  return fitter_result.params
+
+def cross_validate(obs_data, model=MODEL, sim_time=SIM_TIME,
+    num_points=NUM_POINTS, parameters=PARAMETERS,
+    noise_std=0.5, num_folds=3):
+  """
+  Performs cross validation on an antimony model.
+  :param ndarray obs_data: data to fit; columns are species; rows are time instances
+  :param str model: antimony model
+  :param int sim_time: length of simulation run
+  :param int num_points: number of time points produced.
+  :param lmfit.Parameters: parameters to be estimated
+  :return list-lmfit.Parameters, list-float: parameters and RSQ from folds
+  """
+  # Iterate for for folds
+  fold_generator = foldGenerator(num_points, num_folds)  # Create the iterator object
+  result_parameters = []
+  result_rsqs = []
+  for train_indices, test_indices in fold_generator:
+    # This function is defined inside the loop because it references a loop variable
+    new_parameters = parameters.copy()
+    fitter_result = fit(obs_data, model=MODEL,
+      indices=None, parameters=new_parameters,
+      sim_time=SIM_TIME, num_points=NUM_POINTS,
+      road_runner=ROAD_RUNNER)
+    result_parameters.append(fitter_result.params)
+    # Run the simulation using
+    # the parameters estimated using the training data.
+    test_estimates = runSimulation(sim_time, num_points,
+        model=model, road_runner=road_runner,
+        parameters=fitter_result.params)
+    test_estimates = test_estimates[:, 1:]
+    # Calculate RSQ
+    rsq = calcRsq(obs_data, test_estimates, indices=test_indices)
+    result_rsqs.append(rsq)
+  return result_parameters, result_rsqs
---
+++ lectures/lecture_10/test_model_fitting.py
@@ -0,0 +1,80 @@
+'''Tests for model fitting.'''
+
+import model_fitting
+
+import numpy as np
+import lmfit
+
+############ CONSTANTS #############
+NROWS = 10
+NROWS_SUBSET = 5
+NCOLS = 3
+LENGTH = NROWS*NCOLS
+INDICES = range(NROWS)
+# Set to values used in model_fitting.MODEL
+TEST_PARAMETERS = lmfit.Parameters()
+TEST_PARAMETERS.add('k1', value=0.1, min=0, max=10)
+TEST_PARAMETERS.add('k2', value=0.2, min=0, max=10)
+
+def makeData(nrows, ncols, valFunc=None):
+  """
+  Creates an array in the desired shape.
+  :param int nrows:
+  :param int ncols:
+  :param Function value: Function
+                      argument: number of values
+                      return: iterable of values
+  """
+  length = nrows*ncols
+  if valFunc is None:
+    valFunc = lambda v: range(v)
+  data = valFunc(length)
+  matrix = np.reshape(data, (nrows, ncols))
+  return matrix
+  
+
+def testReshapeData():
+  data = makeData(NROWS, NCOLS)
+  array = model_fitting.reshapeData(data, range(NROWS_SUBSET))
+  assert(len(array) == NROWS_SUBSET*NCOLS)
+  assert(np.shape(array)[0] == len(array))
+
+def testArrayDifference():
+  matrix1 =  makeData(NROWS, NCOLS)
+  matrix2 =  makeData(NROWS, NCOLS)
+  array = model_fitting.arrayDifference(matrix1, matrix2, INDICES)
+  assert(sum(np.abs(array)) == 0)
+
+def testCalcRsq():
+  std = 0.5
+  residuals = np.reshape(np.random.normal(0, std, LENGTH),
+      (NROWS, NCOLS))
+  matrix1 =  makeData(NROWS, NCOLS)
+  matrix2 = matrix1 + residuals
+  rsq = model_fitting.calcRsq(matrix2, matrix1)
+  var_est = (1 - rsq)*np.var(matrix1)
+  var_exp = std*std
+  assert(np.abs(var_est - var_exp) < 0.5)
+
+def testMakeParameters():
+  parameters = model_fitting.makeParameters(model_fitting.CONSTANTS)
+  assert(len(parameters.valuesdict()) == len(model_fitting.CONSTANTS))
+ 
+def testRunSimulation():
+  data1 = model_fitting.runSimulation() 
+  assert(data1[-1, 0] == model_fitting.SIM_TIME)
+  data2 = model_fitting.runSimulation(
+      parameters=TEST_PARAMETERS) 
+  nrows, ncols = np.shape(data1)
+  for i in range(nrows):
+    for j in range(ncols):
+      assert(np.isclose(data1[i,j], data2[i,j]))
+  
+  
+if __name__ == '__main__':
+  testReshapeData() 
+  testArrayDifference() 
+  testCalcRsq()
+  testMakeParameters()
+  testRunSimulation()
+  print("OK")
